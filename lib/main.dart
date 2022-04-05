386d4571bd788b03fd2e46be5ea454ac3fb2bfbe
okaryo <okaryotr@gmail.com>
2022-04-06 01:25:33 +0900

    [modify] Main to add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:curl_generator/view/main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
