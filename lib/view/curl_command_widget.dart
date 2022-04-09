4d047b2d29572b4ef92bbf6f57187dcf94ee596f
okaryo <okaryotr@gmail.com>
2022-04-10 01:30:42 +0900

    [modify] command widget to add copy icon

--- lib/view/curl_command_widget.dart
+++ lib/view/curl_command_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CurlCommandWidget extends StatelessWidget {
   final String _command;
@@ -7,17 +8,35 @@ class CurlCommandWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.black,
-      ),
-      child: Text(
-        '\$ $_command',
-        style: const TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 16),
-      ),
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.black,
+          ),
+          child: Text(
+            '\$ $_command',
+            style: const TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        Positioned(
+          right: 16,
+          child: Center(child: _buildCopyIcon()),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCopyIcon() {
+    return IconButton(
+      padding: EdgeInsets.zero,
+      constraints: const BoxConstraints(),
+      onPressed: () => Clipboard.setData(ClipboardData(text: _command)),
+      icon: const Icon(Icons.copy, color: Colors.white),
     );
   }
 }
