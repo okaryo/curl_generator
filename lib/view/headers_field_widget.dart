12800d30cf41df330f9182fdf8d25806cdff9241
okaryo <okaryotr@gmail.com>
2022-04-10 15:51:28 +0900

    [modify] class name

--- lib/view/headers_field_widget.dart
+++ lib/view/headers_field_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HeaderFieldWidget extends StatelessWidget {
+class HeadersFieldWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text('under development');
--- lib/view/options_widget.dart
+++ lib/view/options_widget.dart
@@ -1,10 +1,10 @@
 import 'package:curl_generator/view/body_field_widget.dart';
-import 'package:curl_generator/view/header_field_widget.dart';
+import 'package:curl_generator/view/headers_field_widget.dart';
 import 'package:curl_generator/view/params_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum Tab { params, header, body }
+enum Tab { params, headers, body }
 
 final selectedTabProvider = StateProvider<Tab>((_) => Tab.params);
 
@@ -16,7 +16,7 @@ class OptionsWidget extends StatelessWidget {
         children: [
           Row(children: [
             _buildTab(Tab.params),
-            _buildTab(Tab.header),
+            _buildTab(Tab.headers),
             _buildTab(Tab.body),
           ]),
           _buildTabContent(),
@@ -33,8 +33,8 @@ class OptionsWidget extends StatelessWidget {
         case Tab.params:
           selectedContent = ParamsFieldWidget();
           break;
-        case Tab.header:
-          selectedContent = HeaderFieldWidget();
+        case Tab.headers:
+          selectedContent = HeadersFieldWidget();
           break;
         case Tab.body:
           selectedContent = BodyFieldWidget();
