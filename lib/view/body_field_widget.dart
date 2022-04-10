9819d7fa2b5153b9b1a8dadd1504b47392be85bf
okaryo <okaryotr@gmail.com>
2022-04-10 12:31:39 +0900

    [add] field widget

---
+++ lib/view/body_field_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BodyFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('under development');
+  }
+}
---
+++ lib/view/header_field_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HeaderFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('under development');
+  }
+}
